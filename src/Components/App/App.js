2aa04adefa6da8e073c7b4506a27d3c82e91d79e
kmbarnett <kmbarnett94@gmail.com>
2020-05-26 16:37:14 -0600

    Add logo and changed div to main

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import JoinForm from '../JoinForm/JoinForm.js';
 import RoomForm from '../RoomForm/RoomForm.js';
 import { Route } from 'react-router-dom';
+import {ReactComponent as ChatLogo} from './assets/message.svg'
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
+      <header>
+        <div className='logo-box'>
+          <ChatLogo />
+          <div className='dash-circle'></div>
+        </div>
+        <h1>Circle Chat</h1>
+      </header>
       <Route path='/' exact>
         <JoinForm />
       </Route>
       <Route path='/create' exact>
         <RoomForm />
       </Route>
-    </div>
+    </main>
   );
 }
 
