5ba01819dc07f0c36c8770dd99859a5142277eb9
Ezekiel Clark <clarkzeke@gmail.com>
2020-05-27 17:03:40 -0600

    Set up Provider
    Co-Authored-By: Kyle Barnett <KmBarnett@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -9,12 +9,16 @@ import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './Reducers';
 
+const store = createStore(rootReducer, composeWithDevTools())
+
+
+
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
+  <Provider store={ store }>
+    <Router>
       <App />
-    </React.StrictMode>
-  </Router>,
+    </Router>,
+  </Provider>
   document.getElementById('root')
 );
 
