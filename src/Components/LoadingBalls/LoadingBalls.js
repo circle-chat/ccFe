e623ae845097a033cfb492d300a183643bceb63c
Ezekiel Clark <clarkzeke@gmail.com>
2020-05-29 14:51:57 -0600

    add loading balls

---
+++ src/Components/LoadingBalls/LoadingBalls.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import './LoadingBalls.css';
+
+function LoadingBalls() {
+  return (
+    <div class="loader" id="loader-4"> 
+      <span></span> 
+      <span></span> 
+      <span></span> 
+    </div> 
+  )
+}
+
+export default LoadingBalls
