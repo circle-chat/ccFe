14e719803245468c2d6a771d20d1c06122a0234d
Ezekiel Clark <clarkzeke@gmail.com>
2020-05-29 12:31:29 -0600

    rebase merge conflicts

--- src/Components/ChatDisplay/ChatDisplay.js
+++ src/Components/ChatDisplay/ChatDisplay.js
@@ -1,31 +1,24 @@
 import React, { useState, useEffect, Component } from 'react';
-import './../ChatContainer/ChatContainer.css';
-import { Link } from 'react-router-dom';
-import ChatContainer from './../ChatContainer/ChatContainer.js'
-import io from 'socket.io-client';   
+import './ChatDisplay.css';
 
-const endPoint = "http://localhost:5000";  
-const socket = io.connect(`${endPoint}`);
+function ChatDisplay({ messages }) {
 
-class ChatDisplay extends React.Component {
-  render() {
-    return (
-      <ul className="ChatDisplay">
-        {this.props.messages.map(message => {
-          return (
-            <li key={message.id}>
-              <div>
-                {message.senderId}
-              </div>
-              <div>
-                {message.text}
-              </div>
-            </li>
-          )
-        })}
-      </ul>
-    )
-  }
+  return (
+    <ul className="ChatDisplay">
+      { messages.map(message => {
+        return (
+          <li key={message.id}>
+            <div>
+              {message.senderName}
+            </div>
+            <div>
+              {message.text}
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  );
 }
 
 export default ChatDisplay;
