7f91acd91d8eda916f7f9e8d8ac3007097dbf647
kmbarnett <kmbarnett94@gmail.com>
2020-06-01 09:07:14 -0600

    Add transition animiation

--- src/Components/Participant/Participant.js
+++ src/Components/Participant/Participant.js
@@ -4,18 +4,11 @@ import LoadingBalls from './../LoadingBalls/LoadingBalls.js';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 function Participant({ waiting, userTwo, group }) {
-//
+
   const displayParticipant = ( message ) => {
     return (
       <section>
-        <section className='code-section'>
           <h3>Circle Code: { group }</h3>
-          <CopyToClipboard text={`${window.location.origin}/join/${group}`}>
-            <button id='icon-button' data-tooltip="Copy Invite Link.">
-              <img className='icon' src="assets/clipboard.svg" alt="clipboard" />
-            </button>
-          </CopyToClipboard>
-        </section>
         { message }
         <h3 className='user-two'>{ userTwo }</h3>
       </section>
@@ -31,10 +24,22 @@ function Participant({ waiting, userTwo, group }) {
 
   const connectedMessage = <h3 className='in-message'>Connected With:</h3>
 
+  useEffect(() => {
+    if (userTwo) {
+      let participant = document.querySelector('#Participant')
+      participant.classList.add('connected')
+    }
+  }, [userTwo])
+
   return (
-    <section className="Participant">
+    <section id='Participant' className='Participant'>
       { !waiting && displayParticipant( connectedMessage ) }
       { waiting && displayParticipant( waitingMessage ) }
+      <CopyToClipboard text={`${window.location.origin}/join/${group}`}>
+        <button id='icon-button' data-tooltip="Copy Invite Link.">
+          <img className='icon' src="assets/clipboard.svg" alt="clipboard" />
+        </button>
+      </CopyToClipboard>
     </section>
   );
 }
