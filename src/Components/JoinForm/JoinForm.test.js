2098089ac3c955e88287ff53391afcd6131a1782
kmbarnett <kmbarnett94@gmail.com>
2020-06-01 11:47:04 -0600

    Add testing to join from
    Co-Authored-By: Zeke <Yetidancer@users.noreply.github.com>

---
+++ src/Components/JoinForm/JoinForm.test.js
@@ -0,0 +1,75 @@
+import React from "react";
+import JoinForm from "./JoinForm";
+import io from "socket.io-client";
+import { render, waitFor, fireEvent, act } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../reducers';
+import { MemoryRouter as Router } from 'react-router-dom';
+
+
+const testStore = createStore(rootReducer);
+
+
+
+
+function renderJoinForm() {
+  return render(
+    <Router>
+      <Provider store={testStore}>
+        <JoinForm  />
+      </Provider>
+    </Router>
+  )
+}
+
+describe("<JoinForm />", () => {
+  it('Renders the from', () => {
+    const { getByText, getByPlaceholderText } = renderJoinForm()
+
+    const nameInput = getByPlaceholderText('Name')
+    const codeInput = getByPlaceholderText('Group Code')
+    const chatButton = getByText('Chat!')
+    const createButton = getByText('Create Group')
+
+    expect(nameInput).toBeInTheDocument()
+    expect(codeInput).toBeInTheDocument()
+    expect(chatButton).toBeInTheDocument()
+    expect(createButton).toBeInTheDocument()
+  })
+
+  it('Renders the from', () => {
+    const { getByText, getByPlaceholderText } = renderJoinForm()
+
+    const nameInput = getByPlaceholderText('Name')
+    const codeInput = getByPlaceholderText('Group Code')
+    const chatButton = getByText('Chat!')
+
+
+    fireEvent.change(nameInput, { target: { value: 'Test-Name' } })
+    fireEvent.change(codeInput, { target: { value: 'Test-Code' } })
+
+    expect(chatButton.disabled).toBe(false)
+
+    fireEvent.click(chatButton)
+
+    const expected = ['Test-Code']
+    expect(JSON.parse(window.localStorage.codes)).toEqual(
+      expect.arrayContaining(expected),
+    );
+
+  })
+
+  it('chat disabled if form is not filled out', () => {
+    const { getByText, getByPlaceholderText } = renderJoinForm()
+
+
+    const chatButton = getByText('Chat!')
+
+
+    expect(chatButton.disabled).toBe(true)
+
+  })
+
+});
